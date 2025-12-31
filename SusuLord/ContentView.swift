c783ec74335bd8cce3a504253b8fa181ca61fa24
susu <78097656@qq.com>
2025-12-31 21:41:26 +0800

    Add developer signature

--- SusuLord/ContentView.swift
+++ SusuLord/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+
+            Text("Developed by 苏苏")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
         }
         .padding()
     }
