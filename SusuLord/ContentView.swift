e95cf1306e44fd6080a3c8d341f3557ecc2be085
苏苏 <78097656@>
2025-12-31 16:39:59 +0800

    Initial Commit

---
+++ SusuLord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SusuLord
+//
+//  Created by suyinghui on 2025/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SusuLord/SusuLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SusuLordApp.swift
+//  SusuLord
+//
+//  Created by suyinghui on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct SusuLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
